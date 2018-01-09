c633a96670b366e98d80b7642ed220d76decc0dd
TeeNik <teenik3315@gmail.com>
2018-01-10 01:39:59 +0300

    open windows

---
+++ Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+
+    public Transform Window;
+    public void OnClick()
+    {
+        Window.DOLocalMoveX(0, 1);
+    }
+}
---
+++ Assets/Scripts/UI/WindowManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowManager : MonoBehaviour {
+
+    enum WindowType
+    {
+        Update
+    }
+
+    public GameObject[] Windows;
+
+    public Transform lastPos;
+
+    public static OpenW
+
+}
